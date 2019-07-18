46a108778583513fb45904d87b84ca84f1957b63
atrangg <52976479+atrangg@users.noreply.github.com>
2019-07-17 23:19:22 -0600

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,76 @@
+/*
+ * Stack Class - methods used for stack
+ * 
+ * @author Trang, Anthony
+ * @version 1
+ * 
+ */
+
+public class Stack<T> {
+
+	LList<T> link = new LList<T>(); // Instantiate class LList
+	
+	// ********************************************************************
+	
+	/*
+	 * push method - pushes the first element into the link
+	 */
+	
+	public void push(T data) {
+		
+		link.addFirst(data);
+		
+	} // End push
+	
+	// ********************************************************************
+
+	/*
+	 * pop method - pops the first element out of the link
+	 */
+	
+	public void pop() {
+		
+		link.removeFirst();
+		
+	} // End pop
+	
+	// ********************************************************************
+
+	/*
+	 * peek method - Displays the first element on the top of the link.
+	 */
+	public void peek() {
+		
+		link.first();
+		
+	} // End peek
+	
+	// ********************************************************************
+
+	/*
+	 * isEmpty - Checks if the stack is empty.
+	 * 
+	 * @return if the stack is empty.
+	 */
+	
+	public boolean isEmpty() {
+		
+		return link.isEmpty();
+		
+	} // end isEmpty
+	
+	// ********************************************************************
+
+	/*
+	 * clear method - Clears the stack elements
+	 * 
+	 */
+	
+	public void clear() {
+		
+		link.clear();
+		
+	} // End clear
+	
+	
+} // End Stack class
